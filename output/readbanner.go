0786da2ad8b7154175ac23422b8806142eedeb09
oshowunm <oluwaseunshowunmi97@gmail.com>
2026-04-01 15:53:09 +0100

    feat/readbannerfunc

---
+++ output/readbanner.go
@@ -0,0 +1,34 @@
+package asciiartoutput
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func readBanner(banner string) []string {
+	if banner != "standard" && banner != "thinkertoy" && banner != "shadow" {
+		fmt.Println("Error: Wrong Banner name")
+		return nil
+	}
+
+	filename := banner + ".txt"
+
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("Error Opening file", err)
+		return nil
+	}
+
+	defer file.Close()
+
+	readString := bufio.NewScanner(file)
+
+	var keepString []string
+
+	for readString.Scan() {
+		keepString = append(keepString, readString.Text())
+	}
+
+	return keepString
+}
